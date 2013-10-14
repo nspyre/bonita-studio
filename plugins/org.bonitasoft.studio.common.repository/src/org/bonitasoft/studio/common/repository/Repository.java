80c670647181f9099d9ac867d5bcc8c6cf421d9e
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-10-14 16:36:57 +0200

    STUDIO-3161

--- plugins/org.bonitasoft.studio.common.repository/src/org/bonitasoft/studio/common/repository/Repository.java
+++ plugins/org.bonitasoft.studio.common.repository/src/org/bonitasoft/studio/common/repository/Repository.java
@@ -77,6 +77,7 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.jobs.IJobManager;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.emf.edapt.migration.MigrationException;
 import org.eclipse.jdt.core.IClasspathEntry;
@@ -421,7 +422,10 @@ public class Repository implements IRepository {
 					//Took example from JDT configure Build path dialog
 					CPListElement[] existingCPElement =  CPListElement.createFromExisting(javaProject);
 					BuildPathsBlock.flush(new ArrayList<CPListElement>(Arrays.asList(existingCPElement)),javaProject.getOutputLocation(), javaProject, null, monitor);
-					getProject().build(IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor);
+					getProject().build(IncrementalProjectBuilder.CLEAN_BUILD, monitor);
+					IJobManager jobManager = Job.getJobManager(); 
+					jobManager.join(ResourcesPlugin.FAMILY_AUTO_BUILD, monitor);
+					jobManager.join(ResourcesPlugin.FAMILY_MANUAL_BUILD, monitor);
 				}
 			} catch (Exception ex) {
 				BonitaStudioLog.error(ex);
