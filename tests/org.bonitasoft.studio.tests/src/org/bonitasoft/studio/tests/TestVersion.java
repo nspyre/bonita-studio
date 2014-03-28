efc8c06dfb699d0197be4b0a1b3b44245bab8534
CI User <ci@ci-62x>
2014-03-28 14:19:25 +0100

    New trunk snapshot from ci

--- tests/org.bonitasoft.studio.tests/src/org/bonitasoft/studio/tests/TestVersion.java
+++ tests/org.bonitasoft.studio.tests/src/org/bonitasoft/studio/tests/TestVersion.java
@@ -31,7 +31,7 @@ import org.eclipse.ui.PlatformUI;
 public class TestVersion extends TestCase {
 
 	public void testNewProcessVersionMatchesProduct() throws Exception {
-		String version = "6.2.6";//TO BE MODIFIED AT EACH RELEASE
+		String version = "6.2.7";
 		NewDiagramCommandHandler command = new NewDiagramCommandHandler();
 		command.execute(null);
 		ProcessDiagramEditor editor = (ProcessDiagramEditor)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
